a5412c1cda6fe3ec2ade8e4162c79ac65b9bd40a
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2022-06-02 16:03:52 -0300

    feat(get-emails-service): get emails service added to the application, now we can list a specific email or all the emails

---
+++ src/controllers/ListEmailsController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { AppError } from "../errors/AppError";
+import { GetAllOrReturnOneEmailService } from "../services/GetAllOrReturnOneEmailService";
+
+class ListEmailsController {
+  async handleGetAllEmails(req: Request, res: Response) {
+    const getAllOrReturnOneEmailService = new GetAllOrReturnOneEmailService();
+
+    const emailsList = await getAllOrReturnOneEmailService.get();
+
+    return res.status(200).json({ emailsList });
+  }
+
+  async handleFetchAnEmailById(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const getAllOrReturnOneEmailService = new GetAllOrReturnOneEmailService();
+
+    if (!id) {
+      throw new AppError("Email id wasn't provided!");
+    }
+
+    const emailToReturn = await getAllOrReturnOneEmailService.get(id);
+
+    return res.status(200).json({ email: emailToReturn });
+  }
+}
+
+export { ListEmailsController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateEmailController } from "./controllers/CreateEmailController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ListEmailsController } from "./controllers/ListEmailsController";
 import { ListUsersController } from "./controllers/ListUsersController";
 
 const routes = Router();
@@ -9,8 +10,11 @@ const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
 
 const createEmailController = new CreateEmailController();
+const listEmailsController = new ListEmailsController();
 
 routes.get("/list-users", listUsersController.handle);
+routes.get("/list-emails", listEmailsController.handleGetAllEmails);
+routes.get("/list-email/:id", listEmailsController.handleFetchAnEmailById);
 
 routes.post("/create-user", createUserController.handle);
 routes.post("/send-email", createEmailController.handle);
